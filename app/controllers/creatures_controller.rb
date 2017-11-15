876a7fa55cd63dd526317f0bec03c2b86d0707a0
lperry34 <leviperry34@gmail.com>
2017-11-15 14:54:35 -0800

    half way

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -2,4 +2,27 @@ class CreaturesController < ApplicationController
   def index
     @creatures = Creature.all
   end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    @creature = Creature.new(creature_params)
+    if @creature.save
+      redirect_to '/creatures'
+    end
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+
+
+  private
+      def creature_params
+        params.require(:creature).permit(:name, :description, :id)
+      end
+
 end
